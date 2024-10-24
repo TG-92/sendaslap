c1df20f773f1b16cf137a57a8072ef288d9e4d91
Alf <gruber.1992@gmx.de>
2024-10-24 16:32:38 +0200

    Accordion eingebunden, loesung hier war npm install react-bootstrap bootstrap, somit war die richtige library für bootsstrap hinterlegt. TG_24_10_24 thx Br.

--- sendaslap/src/App.js
+++ sendaslap/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Rudi/Navbar';
+import Content from './components/layout/content/Content';
+
 import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Content/> 
       <Footer/>
     </div>
   );
