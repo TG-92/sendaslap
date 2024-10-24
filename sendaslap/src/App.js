652cd1d39ba7b7e775c9a7ea9c1ff4fd675b4207
RKoleda <R.Koleda@gmx.de>
2024-10-24 13:46:16 +0200

    App.js wiederhergestellt

--- sendaslap/src/App.js
+++ sendaslap/src/App.js
@@ -1,32 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Rudi/Header'; 
 
 function App() {
   return (
     <div className="App">
-      
-      <Header />
-
-      
-      <main>
-        <section id="home">
-          <h1>Welcome to Home</h1>
-          <p>This is the home section of the app.</p>
-        </section>
-        <section id="about">
-          <h1>About Us</h1>
-          <p>Some information about us.</p>
-        </section>
-        <section id="services">
-          <h1>Our Services</h1>
-          <p>Details of services we provide.</p>
-        </section>
-        <section id="contact">
-          <h1>Contact Us</h1>
-          <p>Ways to contact us.</p>
-        </section>
-      </main>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
