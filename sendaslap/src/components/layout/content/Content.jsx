b87144f48bc9274becc289247041713b39019f1f
Alf <gruber.1992@gmx.de>
2024-10-24 19:08:58 +0200

    breaks in den Text eingefügt. TG_24_10_2024

--- sendaslap/src/components/layout/content/Content.jsx
+++ sendaslap/src/components/layout/content/Content.jsx
@@ -8,10 +8,12 @@ function Content() {
       <Accordion.Item eventKey="0">
         <Accordion.Header>What The Slap?!</Accordion.Header>
         <Accordion.Body>
-          Dont you just sometimes think you're not living up to your true potential?
-          Maybe you're just not living up to your dreams. But remember:
-          Never stop letting people not help, just give them the tools they need to help them grow.
-          And remember: You're never truly alone. We are literally one Hand away. 
+          Dont you just sometimes think you're not living up to your true potential? <br />
+          Maybe you're just not living up to your dreams. But remember: <br />
+          Never stop letting people not help, just give them the tools they need to help them grow. <br />
+          And remember: You're never truly alone. <br />
+          - We are literally just -  <br /> 
+          -one Hand away - <br />
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
@@ -24,7 +26,6 @@ function Content() {
           And over the next 12 centuries we've installed slapboxes in every major city in the world.
           We've worked with hundreds of celebrities and we've seen the results. 
           Check out our Website and find your perfect Slap Story!
-
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
