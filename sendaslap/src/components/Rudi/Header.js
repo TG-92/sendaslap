1414aac92fae5171ed51fd4d6d16a853fbe302be
RKoleda <R.Koleda@gmx.de>
2024-10-24 14:03:00 +0200

    Navbar hinzugefuegt -RK

--- sendaslap/src/components/Rudi/Header.js
+++ sendaslap/src/components/Rudi/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <nav className="nav">
+      <nav className="navbar">
         <div className="logo">
           <a href="#home">MyLogo</a>
         </div>
