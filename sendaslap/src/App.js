21e219ac400e6eb12138db8d9a169cf8e8f28c3f
Alf <gruber.1992@gmx.de>
2024-10-25 12:49:04 +0200

    buttonred ordner erstellt und komponente erstellt, beim npm start aufruf aber fehler

--- sendaslap/src/App.js
+++ sendaslap/src/App.js
@@ -2,14 +2,15 @@
 import './App.css';
 import Navbar from './components/Rudi/Navbar';
 import Content from './components/layout/content/Content';
-
+import Buttonred from './components/layout/buttonred/Buttonred';
 import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Content/> 
+      <Content/>
+      <Buttonred/>
       <Footer/>
     </div>
   );
---
+++ sendaslap/src/components/common/buttonred/button.jsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+function buttonred() {
+    return (
+        <>
+        <Button variant="primary">Info</Button>
+        </>
+    );
+}
+
+export default buttonred;
